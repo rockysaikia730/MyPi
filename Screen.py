b1ff3af98e8aa6ef8709eaaeb1e650c67480517b
A.R.S <67681828+rockysaikia730@users.noreply.github.com>
2022-03-13 06:56:48 +0530

    Create Screen.py

---
+++ Screen.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+#Create an instance of tkinter frame
+win = Tk()
+
+#Set the geometry
+win.geometry("1920x1080")
+
+#Create a fullscreen window
+win.attributes('-fullscreen', True)
+
+win.mainloop()
